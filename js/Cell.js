a8f7482bf0099365185d05dde23112b47a5021f5
jjuarez2902 <jjuarez@albany.edu>
2018-06-25 15:44:07 -0400

    Added more classes .js files :kangaroo:

---
+++ js/Cell.js
@@ -0,0 +1,3 @@
+// Jorge Juarez - Project 0 : Scrabble Game
+// .js file used in conjunction with ../views/index.html
+// .js file houses the Cell object, which is the main Object used to check the status of the game board
--- js/Tile.js
+++ js/Tile.js
@@ -1,3 +1,3 @@
 // Jorge Juarez - Project 0: Scrabble Game
 // .js file used in conjunction with ../views/index.html
-// .js file to house Tile object, which is the main Object to be used to update status of Game Board
+// .js file to house Tile object, which is the tiles contained in a player's hand or deck
