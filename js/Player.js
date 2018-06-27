ad2f8bcdac1c29ed907c4e59bd7b8268cee0da00
jjuarez2902 <jjuarez@albany.edu>
2018-06-26 21:32:46 -0400

    Added Player.js class :watermelon:

---
+++ js/Player.js
@@ -0,0 +1,23 @@
+// Jorge Juarez - Project 0 : Scrabble Game
+// .js file used in conjunction with ../views/index.html
+// .js file houses the Player object, which will hold the current scores and such for the game
+
+/* Anatomy of the Player class:
+    idSymbol: number type, just is a plain id to get  a Player 1 or 2
+    scoreSymbol: number type, tracks the current score of the Player
+*/
+let Player = (
+    function(){
+        let idSymbol = Symbol['id'];
+        let scoreSymbol = Symbol['score'];
+        function Player(id = 1, score = 0){
+            this[idSymbol] = id;
+            this[scoreSymbol] = score;
+        }
+        Player.prototype.getId = function() { return this[idSymbol]; }; // getters
+        Player.prototype.getScore = function() { return this[scoreSymbol]; };
+        Player.prototype.setId = function(id) { this[idSymbol] = id; }; // setters
+        Player.prototype.setScore = function(score) { this[scoreSymbol]  = score; }
+        return Player;
+    }
+)();
